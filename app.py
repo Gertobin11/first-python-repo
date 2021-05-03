9b66513a57bdb6b514f2553f300d7d849d0782a7
Gertobin11 <gertobin11@gmail.com>
2021-05-03 23:33:19 +0000

    added contact form and completed member page

--- app.py
+++ app.py
@@ -19,6 +19,17 @@ def about():
     return render_template("about.html", page_title="About", company=data)
 
 
+@app.route("/about/<members_name>")
+def about_member(members_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == members_name:
+                member = obj
+    return render_template("member.html", member=member)
+
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html", page_title="Contact")
